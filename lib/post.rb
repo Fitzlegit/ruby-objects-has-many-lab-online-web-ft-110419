1bf26dbbb6e37617e63fd5ec6464c0c17a06b42f
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-24 02:07:08 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+class Post 
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def author_name
+    #binding.pry
+    if !author.name.empty?
+      author.name
+    end
+  end
+    
+  
+  
+end
